70a2db28cd1459caa9a467171f79e89416b42305
Martin Albedinsky <martin.albedinsky@gmail.com>
2017-02-21 22:47:11 +0100

    Updated mode for permissions.sh file.

---
+++ ci/deploy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Execute Bintray deploy task (see master branch)
+# ./gradlew :library:bintrayUpload
---
+++ ci/permissions.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+chmod +x ./gradlew
+chmod +x ./ci/tests.sh
+chmod +x ./ci/coverage.sh
+chmod +x ./ci/deploy.sh
